828a686ef63d644a007d00eeabfbcf178253edf1
Ridho Perdana <ridho.perdana@wartek.belajar.id>
2025-02-28 14:26:10 +0700

    allow popup seen

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -241,26 +241,10 @@ static CDVWKInAppBrowser* instance = nil;
     
     __weak CDVWKInAppBrowser* weakSelf = self;
     
-    // Run later to avoid the "took a long time" log message.
+    // MODIFY THIS SECTION
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (weakSelf.inAppBrowserViewController != nil) {
-            float osVersion = [[[UIDevice currentDevice] systemVersion] floatValue];
-            __strong __typeof(weakSelf) strongSelf = weakSelf;
-            if (!strongSelf->tmpWindow) {
-                CGRect frame = [[UIScreen mainScreen] bounds];
-                if(initHidden && osVersion < 11){
-                   frame.origin.x = -10000;
-                }
-                strongSelf->tmpWindow = [[UIWindow alloc] initWithFrame:frame];
-            }
-            UIViewController *tmpController = [[UIViewController alloc] init];
-            [strongSelf->tmpWindow setRootViewController:tmpController];
-            [strongSelf->tmpWindow setWindowLevel:UIWindowLevelNormal];
-
-            if(!initHidden || osVersion < 11){
-                [self->tmpWindow makeKeyAndVisible];
-            }
-            [tmpController presentViewController:nav animated:!noAnimate completion:nil];
+        if (self.viewController != nil) {
+            [self.viewController presentViewController:nav animated:!noAnimate completion:nil];
         }
     });
 }
